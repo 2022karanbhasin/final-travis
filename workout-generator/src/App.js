396597b029183f6766cc3842b541f97ed27303d2
2022karanbhasin <44887140+2022karanbhasin@users.noreply.github.com>
2020-03-10 18:54:12 -0500

    passing test of interaction

--- workout-generator/src/App.js
+++ workout-generator/src/App.js
@@ -5,7 +5,6 @@ import SplashPage from './Pages/SplashPage';
 import WorkoutsPage from './Pages/WorkoutsPage';
 import TopMenu from './Components/TopMenu';
 import ProfilePage from './Pages/ProfilePage';
-import { WorkoutContext } from './context';
 import { firebase }from './firebaseDb';
 import 'firebase/database';
 import "firebase/auth";
@@ -17,15 +16,6 @@ const App = () => {
     firebase.auth().onAuthStateChanged(setUser);
   }, []);
   
-
-  const workoutContext = useContext(WorkoutContext)
-  const { equipment, workouts, countdown, muscleGroups } = workoutContext;
-  console.log('duration', countdown)
-  console.log('equip', equipment)
-  console.log('muscle groups', muscleGroups)
-  console.log('workout list', workouts)
-  console.log(user)
-
   const withMenu = (page) => {
     return (
       <Container>
